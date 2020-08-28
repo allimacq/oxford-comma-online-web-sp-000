c9016c8b6581567584bc53782035f127f201c876
Allison MacQueen <allimacq@gmail.com>
2020-08-28 23:49:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     new = array.join(", ")
     return new
   elsif array.length > 1 && array.length < 6
-    new = array.insert(-2, ", and")
+    new = array.insert(-2, " and")
     final = new.join(", ")
     puts final
   end
