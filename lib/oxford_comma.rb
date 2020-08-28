f8acded59dec74edc12fe4aed3c6b720e4a344bf
Allison MacQueen <allimacq@gmail.com>
2020-08-28 23:23:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  i = 0
+  new = Array.new()
+  length = array.length()
+  while i < 5
+    if i < array.length 
+      new = array.join(", ")
+      puts new
+      i += 1
+      puts "#{i} WANT TO KNOW WHERE I AM"
+    end
+  end
 end
+
+
+
+#do length -1 and then add the last one by adding and before it  
